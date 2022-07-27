673a61cc90ba3a3b634b45009b3948767a1453f2
kaushik <kaushiksheel9@gmail.com>
2022-07-27 11:36:29 +0530

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import Link from '../assets/link.svg';
 import Github from '../assets/github.svg';
 import {urlFor} from '../client';
+import {motion } from 'framer-motion';
 
 export const Card=({demo,github,title,image,desc})=>{
 
@@ -8,7 +9,15 @@ export const Card=({demo,github,title,image,desc})=>{
 
 
     return (
-      <div className="card">
+      <motion.div
+      whileInView={{
+        x: [0, 0],
+        y: [100, 0],
+        opacity: [0, 1],
+      }}
+      transition={{ duration: 0.5, type: "spring" }}
+      
+      className="card">
         <div className="thumbnail">
             <img src={urlFor(image)} alt="project thumbnail" />
             <div className="links">
@@ -24,6 +33,6 @@ export const Card=({demo,github,title,image,desc})=>{
             <p className="title">{title}</p>
             <p className="info">{desc}</p>
         </div>
-      </div>
+      </motion.div>
     )
 }
